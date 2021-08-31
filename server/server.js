c5889d2691794f48514c82f63826fe320ad8f4d0
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 10:49:54 -0500

    cleaning up server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const dotenv = require("dotenv")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
