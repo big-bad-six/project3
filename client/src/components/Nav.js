927378f457dd5ddb5cc52c62031c5df1297fc02b
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 11:23:48 -0500

    populated nav and footer components

---
+++ client/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <h1>Meme Generator</h1>
+            <div className="links">
+                <a href="/">Log In/Sign Up</a>
+            </div>
+        </nav>
+    )
+}
