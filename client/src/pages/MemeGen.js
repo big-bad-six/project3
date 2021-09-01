4de08c57d3c727a63e0d8dca4ab8600f56373222
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 20:05:20 -0500

    creating styles for home page

--- client/src/pages/MemeGen.js
+++ client/src/pages/MemeGen.js
@@ -3,6 +3,10 @@ import React from 'react';
 export default function MemeGen(){
     return (
         <div className="card">
+            <div>
+                <button>Search</button>
+                <button>Create</button>
+            </div>
             <p>Hello World!</p>
         </div>
     );
