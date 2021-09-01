d5ffd18973800e1da9d1401ee0daefcf4bc0a73f
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 20:41:12 -0500

    created mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,31 @@
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const SIGN_UP = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
