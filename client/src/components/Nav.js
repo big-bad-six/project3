67e5ed7783b99335ec3b1a1071f218142347fd19
Uriel Nunez <uanq22@hotmail.com>
2021-08-31 19:59:34 -0500

    Navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './Navbar.css';
 
-/*Building navbar boiler plate links. Add page name on line 10 and 11 */
-export default function Nav({ changePage, curPage }) {
+function Navbar() {
   return (
     <BrowserRouter>
       <Route>
-        <nav className="nav">
-          <h1>Meme Generator</h1>
-          <div className="links">
-            <Link to=""
-              onClick={() => changePage("")}
-              className={
-                curPage === "" ? "nav-link active" : "nav-link"
-              }>Log In/Sign Up</Link>
+        <nav>
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+              MEME GENERATOR
+            </Link>
+            <ul>
+              <li>
+                <Link to='/sign-in' className='nav-links'>
+                  Sign In
+                </Link>
+              </li>
+              <li>
+                <Link to='/sign-up' className='nav-links'>
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
       </Route>
     </BrowserRouter>
   )
 }
+
+export default Navbar
