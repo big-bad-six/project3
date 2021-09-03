3eeaeeceb6954013b65cdabf67f8cab1a7fc8531
Alap Raval <alapsraval@gmail.com>
2021-09-02 22:46:49 -0500

    formatting

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,21 +4,20 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-
-        <nav className="navbar">
-          <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-              MEME GENERATOR
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          MEME GENERATOR
+        </Link>
+        <ul>
+          <li>
+            <Link to='/' className='nav-links'>
+              Log In/Sign Up
             </Link>
-            <ul>
-              <li>
-                <Link to='/' className='nav-links'>
-                  Log In/Sign Up
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
