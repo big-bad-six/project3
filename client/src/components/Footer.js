d60f78b3628a12c02044632f2b40bc01f1b3c1d4
Alap Raval <alapsraval@gmail.com>
2021-09-02 21:09:51 -0500

    replaced div with footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
+        <footer>
             <p> Handcrafted memes brought to you by Group 6 - 2021 </p>
-        </div>
+        </footer>
     )
 }
