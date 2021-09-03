ebcd80beb63f271098d90fcd4879cc5f4147acca
Alap Raval <alapsraval@gmail.com>
2021-09-02 21:04:52 -0500

    moved eventlistner inside SignIn function

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -2,6 +2,60 @@ import React from 'react';
 import '.././assets/css/Form.css';
 
 export default function SignIn() {
+
+    function setFormMessage(formElement, type, message) {
+        const messageElement = formElement.querySelector(".form_message");
+
+        messageElement.textContent = message;
+        messageElement.classList.remove("form_message-success", "form_message-error");
+        messageElement.classList.add(`form_message-${type}`);
+    }
+
+    function setInputError(inputElement, message) {
+        inputElement.classList.add("form_input-error");
+        inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
+    }
+
+    function clearInputError(inputElement) {
+        inputElement.classList.remove("form_input-error");
+        inputElement.parentElement.querySelector(".form_input-error-message").textContent = "";
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const loginForm = document.querySelector("#login");
+        const createAccountForm = document.querySelector("#createAccount");
+
+        document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.add("form-hidden");
+            createAccountForm.classList.remove("form-hidden");
+        });
+
+        document.querySelector("#linkLogin").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.remove("form-hidden");
+            createAccountForm.classList.add("form-hidden");
+        });
+
+        loginForm.addEventListener("submit", e => {
+            e.preventDefault();
+
+            setFormMessage(loginForm, "error", "Invalid username/password combination");
+        });
+
+        document.querySelectorAll(".form_input").forEach(inputElement => {
+            inputElement.addEventListener("blur", e => {
+                if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 6) {
+                    setInputError(inputElement, "Username must be at least 6 characters in length");
+                }
+            });
+
+            inputElement.addEventListener("input", e => {
+                clearInputError(inputElement);
+            });
+        });
+    });
+
     return (
         <div class="container">
             <form class="form" id="login">
@@ -47,56 +101,3 @@ export default function SignIn() {
         </div>
     );
 }
-
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form_message");
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form_message-success", "form_message-error");
-    messageElement.classList.add(`form_message-${type}`);
-}
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form_input-error");
-    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form_input-error");
-    inputElement.parentElement.querySelector(".form_input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form-hidden");
-        createAccountForm.classList.remove("form-hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form-hidden");
-        createAccountForm.classList.add("form-hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form_input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 6) {
-                setInputError(inputElement, "Username must be at least 6 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
