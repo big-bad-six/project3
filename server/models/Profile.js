00c59a164e4d431b49148bf9623d8fe30d11785c
Uriel Nunez <uanq22@hotmail.com>
2021-09-07 20:59:12 -0500

    updated profile and resolvers files

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const profileSchema = new Schema({
     name: {
@@ -7,13 +8,31 @@ const profileSchema = new Schema({
         unique: true,
         trim: true,
     },
-    skills: [
-        {
-            type: String,
-            trim: true,
-        },
-    ],
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
 });
+
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const Profile = model('Profile', profileSchema);
 
