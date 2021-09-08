70e703684a3fa379ca3a6c316cc20db18a62a7e2
Charlie Aleck <aleckcharlie@gmail.com>
2021-09-08 16:44:55 -0500

    created Get Memes button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import mainLogo from '../assets/images/MG_Logo_Main.png'
+import mainLogo from '../assets/images/MG_Logo_Main.png';
+import '../assets/css/MemeGen.css'
 
 export default function Home() {
+
+
+
     return (
         <div className="card" >
             <h1> Welcome!</h1>
             <img className='main-logo' src={mainLogo} alt='Mem Gen Main logo'></img>
-            <p>On this site you can search for images and add custom text to them. This is how it works:</p>
-            <ul>
-                <li>Sign up to be able to come back and see your saved memes! It's free!
-                </li>
-                <li>Search for an image to add custom text to it
-                </li>
-                <li>Save it and share it to anyone
-                </li>
-            </ul>
+            <button>Get to the Memes!</button>
+            
         </div>
     );
 }
