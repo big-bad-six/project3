25b2cee20e8ecebaf2fb55833b578fc63eddb4bc
Charlie Aleck <aleckcharlie@gmail.com>
2021-09-09 18:24:39 -0500

    updated mongoose.connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/meme-generator',
+  process.env.MONGODB_URI || "mongodb+srv://atlas:atlasagain@cluster0.vqreu.mongodb.net/project3?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
