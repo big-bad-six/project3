f3891ee24c28bb855d6804af9bdd247e9a811b97
Alap Raval <alapsraval@gmail.com>
2021-09-10 22:24:04 -0500

    show/hide nav links as per the login state

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import '../assets/css/Navbar.css';
+import Auth from "../utils/auth";
 import { Link } from 'react-router-dom';
+import '../assets/css/Navbar.css';
 import logo from '../assets/images/MG_Logo.png';
 
 function Navbar() {
@@ -12,19 +13,30 @@ function Navbar() {
           </img>
         </Link>
         <ul className="main-nav">
-          <li>
-            <Link to='/sign-up' className='nav-links'>
-              Sign Up
-            </Link>
-          </li>
-          <li>
-            <Link to='/log-in' className='nav-links'>
-              Log In
-            </Link>
-          </li>
+          {!Auth.loggedIn() &&
+            <li>
+              <Link to='/sign-up' className='nav-links'>
+                Sign Up
+              </Link>
+            </li>
+          }
+          {!Auth.loggedIn() &&
+            <li>
+              <Link to='/log-in' className='nav-links'>
+                Log In
+              </Link>
+            </li>
+          }
+          {Auth.loggedIn() &&
+            <li>
+              <Link to='/' className='nav-links' onClick={() => Auth.logout()}>
+                Log Out
+              </Link>
+            </li>
+          }
         </ul>
       </div>
-    </nav>
+    </nav >
   )
 }
 
