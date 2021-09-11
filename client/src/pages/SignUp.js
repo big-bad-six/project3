94bd85c53c2161fc07ac9e7d1f8b6ecbadeb3318
Alap Raval <alapsraval@gmail.com>
2021-09-10 21:30:41 -0500

    fixed signup page form handler

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-// import { Link } from 'react-router-dom'
-import '.././assets/css/Form.css';
-import { SIGN_UP } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { Form, Button, Alert } from 'react-bootstrap';
-
+import { SIGN_UP } from '../utils/mutations';
+import '.././assets/css/Form.css';
 
 export default function SignUp(props) {
     const [userFormData, setUserFormData] = useState({ email: '', password: '', firstName: '', lastName: '' });
-    const [validated] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
-    const [addUser, { error }] = useMutation(SIGN_UP);
-
-    useEffect(() => {
-        if (error) {
-            setShowAlert(true);
-        } else {
-            setShowAlert(false);
-        }
-    }, [error]);
+    const [addUser] = useMutation(SIGN_UP);
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -28,44 +15,17 @@ export default function SignUp(props) {
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-        // this is greyed out because it is used in line 24 cost token
-        // we need to create an Auth in utils
-        const form = event.currentTarget;
         try {
             const { data } = await addUser({
                 variables: { ...userFormData },
             });
-            console.log(data);
-            Auth.login(data.addUser.token);
+            const token = data.addUser.token;
+            Auth.login(token);
         } catch (err) {
             console.error(err);
         }
 
-        setUserFormData({
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-        });
-        // const mutationResponse = await addUser({
-        //     variables: {
-        //         email: formState.email,
-        //         password: formState.password,
-        //         firstName: formState.firstName,
-        //         lastName: formState.lastName,
-        //     },
-        // });
-        //const token = mutationResponse.data.addUser.token;
-        // Auth.login(token);
     };
-
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    //     setFormState({
-    //         ...formState,
-    //         [name]: value,
-    //     });
-    // };
 
     return (
         <div className="card">
